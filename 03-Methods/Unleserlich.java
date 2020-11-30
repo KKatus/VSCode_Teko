0819aa6fb6e91986a2aefc02639f9dcfff0745ac
KKatus <sascha.braegger-andorid@hotmail.com>
2020-11-30 23:29:58 +0100

    changes

--- 03-Methods/Unleserlich.java
+++ 03-Methods/Unleserlich.java
@@ -14,32 +14,23 @@ public class Unleserlich {
     public static void main(String args[]) {
         int[] testValues = { 5, 10, 15, 20, 30, 40 };
         for (int i = 0; i < testValues.length; ++i) {
-            System.out.println(funktion1(testValues[i]));
+            System.out.println(fibonacci(testValues[i]));
         }
+
     }
 
     // int a is the value from the array[i]
-    public static int funktion1(int Wert_Array) {
-        int b = 1;
+    public static int fibonacci(int Wert_Array) {
         if (Wert_Array <= 1) {
             if (Wert_Array == 1) {
-                return b;
-            } else {
-                b = addPlusOne(Wert_Array - 1);
+                return Wert_Array;
             }
+            return Wert_Array;
+
         } else {
-            return function2(Wert_Array); // function allways jumps right in
-                                          // here
+            return fibonacci(Wert_Array - 1) + fibonacci(Wert_Array - 2);
         }
-        return b;
+
     }
 
-    public static int addPlusOne(int Wert_Array) {
-        return ++Wert_Array;
-    }
-
-    public static int function2(int Wert_Array) {
-        int container = funktion1(Wert_Array - 1); // wird bis auf 1 gerechnet
-        return container + funktion1(Wert_Array - 2);
-    }
 }
