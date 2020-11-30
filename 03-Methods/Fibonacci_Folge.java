fa974554b0714bb80ebd14d94365fcdf6f61059a
KKatus <sascha.braegger-andorid@hotmail.com>
2020-11-30 13:39:03 +0100

    added the fibonacci method

---
+++ 03-Methods/Fibonacci_Folge.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * Fibonacci-Folge
+ */
+public class Fibonacci_Folge {
+
+    static int result = 0;
+    static int zahl1 = 0;
+    static int zahl2 = 1;
+    static int resultat = 0;
+
+    public static void main(String[] args) {
+
+        System.out.println("Die wievielte Fibonaccizahl soll ausgegeben werden: ");
+        Scanner scanner = new Scanner(System.in);
+        result = scanner.nextInt();
+        scanner.close();
+        System.out.println("Resultat: " + fibonacci(zahl1, zahl2, resultat));
+    }
+
+    public static int fibonacci(int zahl1, int zahl2, int resultat) {
+
+        if (result < 2) {
+            if (result == 1) {
+                resultat = result;
+                return resultat;
+            } else {
+                resultat = result;
+            }
+        } else {
+            for (int i = 2; i <= result; i++) {
+                resultat = zahl1 + zahl2;
+                zahl1 = zahl2;
+                zahl2 = resultat;
+            }
+        }
+
+        return resultat;
+
+    }
+}
