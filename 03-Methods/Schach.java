b3cf8ef1f64326af14bc936b37c725f10591abe2
KKatus <sascha.braegger-andorid@hotmail.com>
2020-12-17 22:11:57 +0100

    serafin code ansehen

---
+++ 03-Methods/Schach.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class Schach {
+    public static HashMap<String, String> SchachMap = new HashMap<String, String>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void getPossibleMovesPawn() {
+
+    }
+
+    public static void getPossibleMovesKing() {
+
+    }
+
+    public static void getPossibleMovesRook() {
+
+    }
+
+    public static void getPossibleMovesBishop() {
+
+    }
+
+    public static void getPossibleMovesQueen() {
+
+    }
+
+    public static void getPossibleMovesKnigh() {
+
+    }
+
+}
